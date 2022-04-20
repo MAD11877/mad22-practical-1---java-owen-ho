f93ca2f1b4453d2a46f6cc81c940f429a1e400a8
owen-ho <93632887+owen-ho@users.noreply.github.com>
2022-04-20 23:45:40 +0800

    Answering Question5
    Finding single mode with loops and dictionary

--- Question5.java
+++ Question5.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.*;
 public class Question5
 {
   public static void main(String[] args)
@@ -25,8 +25,42 @@ public class Question5
      *     2
      * Hint: Use a loop to get input. Use another 2 loops to find the mode
      */
-     
+
+    //get user input
     Scanner in = new Scanner(System.in);
+    System.out.println("How many numbers will you be inputting?");
+    int num = in.nextInt();
+    int[] numList = new int[num];
+    int i=0;
+    while (i<num){
+      numList[i] = in.nextInt();
+      i++;
+    }
+
+    // find occurence of each integer
+    HashMap<Integer, Integer> modeDict = new HashMap<Integer, Integer>();
+    for (int j : numList){
+      if (modeDict.containsKey(j)){
+        modeDict.put(j,modeDict.get(j)+1);
+      }
+      else{
+        modeDict.put(j,1);
+      }
+    }
+
+    // find highest occurence of mode
+    int mostOccurence = 0;
+    for (int k : modeDict.values()){
+      if (k>mostOccurence){
+        mostOccurence = k;
+      }
+    }
     
+    //find key of highest occurence(mode)
+    for(int l : modeDict.keySet()){
+      if(modeDict.get(l)==mostOccurence){
+        System.out.println(l);
+      }
+    }
   }
 }
